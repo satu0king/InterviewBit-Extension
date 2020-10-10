1591568ff140a666c883b3ca15feef577dd40e42
Satvik Ramaprasad <satviksr@gmail.com>
2020-10-10 16:09:51 +0530

    Geeksforgeeks support

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -2,7 +2,14 @@ chrome.extension.sendMessage({}, function(response) {
   var readyStateCheckInterval = setInterval(function() {
     if (document.readyState === 'complete') {
       clearInterval(readyStateCheckInterval);
-      setLeetcodeUrl();
+
+      const leetcodeDataUrl = chrome.runtime.getURL('data/leetcode.json');
+      const leetcodeIconUrl = chrome.runtime.getURL('icons/lc.png');
+      addUrls(leetcodeDataUrl, leetcodeIconUrl);
+
+      const gfgDataUrl = chrome.runtime.getURL('data/gfg.json');
+      const gfgIconUrl = chrome.runtime.getURL('icons/gfg.png');
+      addUrls(gfgDataUrl, gfgIconUrl);
     }
   }, 10);
 });
@@ -14,35 +21,26 @@ const request =
   return json;
 }
 
-const url = chrome.runtime.getURL('data.json');
-const iconUrl = chrome.runtime.getURL('icons/lc.png');
-
-async function setLeetcodeUrl() {
-  var currentUrl = window.location.href;
-  const metadata = await request(url);
-
-  // Problem Page
-  if (currentUrl in metadata) {
-    var leetcodeUrl = metadata[currentUrl];
-    var button = $('<a/>', {text: 'Leetcode Problem'}).attr({
-      type: 'button',
-      style: 'margin-right:10px',
-      target: '_blank',
-      href: leetcodeUrl
-    });
-    button.addClass('pull-right');
-    button.addClass('btn');
-    button.addClass('btn-default');
-    var img = $('<img id="dynamic" class="lc-icon">').attr({'src': iconUrl})
-    button.append(img);
-    $('.problem-action-wrapper').append(button);
+const urlRe = /(https:\/\/www\.interviewbit\.com\/problems\/[^\/]*\/)/;
+function santizeUrl(url) {
+  // Hack
+  if (url.includes('#')) return '';
+  if (urlRe.test(url)) {
+    return url.match(urlRe)[1];
   }
+  return url;
+}
 
-  function addLeetCodeLink() {
-    if (this.href in metadata) {
-      var leetcodeUrl = metadata[this.href];
+async function addUrls(metadatUrl, iconUrl) {
+  var currentUrl = santizeUrl(window.location.href);
+  const metadata = await request(metadatUrl);
+
+  function addLinkHelper() {
+    var url = santizeUrl(this.href);
+    if (url in metadata) {
+      var platformUrl = metadata[url];
       var button = $('<a/>').attr(
-          {style: 'margin-right:10px', target: '_blank', href: leetcodeUrl});
+          {style: 'margin-right:10px', target: '_blank', href: platformUrl});
       var img =
           $('<img id="dynamic" class="lc-icon-big">').attr({'src': iconUrl})
       button.append(img);
@@ -50,5 +48,23 @@ async function setLeetcodeUrl() {
     }
   }
 
-  $('a').each(addLeetCodeLink);
+  // Problem Page
+  if (currentUrl in metadata) {
+    var platformUrl = metadata[currentUrl];
+    var button = $('<a/>').attr({
+      type: 'button',
+      style: 'margin-right:10px',
+      target: '_blank',
+      href: platformUrl
+    });
+    button.addClass('pull-right');
+    button.addClass('btn');
+    button.addClass('btn-default');
+    var img = $('<img id="dynamic" class="lc-icon">').attr({'src': iconUrl})
+    button.append(img);
+	$('.problem-action-wrapper').append(button);
+    setTimeout(() => $('a').each(addLinkHelper), 3000);
+  } else {
+    $('a').each(addLinkHelper);
+  }
 }
